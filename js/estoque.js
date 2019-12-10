872d9337c47a02a0a733653a6aaa05548d898768
Ajax Lima <ajaxlima94@gmail.com>
2019-12-10 11:32:01 -0300

    Inserindo os dados no DB

--- js/estoque.js
+++ js/estoque.js
@@ -22,6 +22,7 @@ function successDB(){}
 // Cria a tabela no BD
 function createDB(tx){
     tx.executeSql('CREATE TABLE IF NOT EXISTS Produtos (id INTEGER PRIMARY KEY, nome VARCHAR(50), qtd INTEGER, preco FLOAT)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Compras (id INTEGER PRIMARY KEY, produto VARCHAR(50), valorCompra FLOAT)');
 }
 
 // Exibe a tela para inserir produto
@@ -29,10 +30,17 @@ function exibirTela(x){
     if(x == 0){
         $("#tela_principal").hide(); // Esconde a tela princiapl
         $("#tela_inserir_produto").show(); // Mostra a tela de insercao de produto
-    }else if(x == 1){
+        return;
+    }
+    if(x == 1){
         $("#tela_principal").hide(); // Esconde a tela princiapl
-    }else if(x == 2){
+        $("#tela_listagem_produtos").show(); // Mostra a tela de listagem de produtos
+        return;
+    }
+    if(x == 2){
         $("#tela_principal").hide(); // Esconde a tela princiapl
+        $("#tela_carrinho").show(); // Mostra a tela do carrinho de compras
+        return;
     }
 }
 
@@ -41,9 +49,35 @@ function fecharTela(x){
     if(x == 0){
         $("#tela_principal").show(); // Mostra a tela princiapl
         $("#tela_inserir_produto").hide(); // Esconde a tela de insercao de produto
-    }else if(x == 1){
-        $("#tela_principal").show(); // Mostra a tela princiapl
-    }else if(x == 2){
-        $("#tela_principal").show(); // Mostra a tela princiapl
+        return;
     }
+    if(x == 1){
+        $("#tela_principal").show(); // Mostra a tela princiapl
+        $("#tela_listagem_produtos").hide(); // Esconde a tela de listagem de produtos
+        return;
+    }
+    if(x == 2){
+        $("#tela_principal").show(); // Mostra a tela princiapl
+        $("#tela_carrinho").hide(); // Esconde a tela do carrinho de compras
+        return;
+    }
+}
+
+// Prepara para inserir dados na tabela Produtos
+function inserirProduto(){
+    db.transaction(estoque_inserirProduto_db, errorDB, successDB);
+}
+
+// Insere na os dados na tabela Produtos
+function estoque_inserirProduto_db(tx){
+    var nome = $("#nome_produto").val();
+    var qtd = $("#qtd_estoque").val();
+    parseInt(qtd);
+    var preco = $("#preco_produto").val();
+    parseFloat(preco);
+
+    tx.executeSql('INSERT INTO Produtos (nome, qtd, preco) VALUES ("' + nome + '", ' + qtd + ', ' + preco + ')');
+    alert("Produto cadastrado com sucesso!");
+    alert( nome + " " + qtd + " " + preco);
+    fecharTela(0);
 }
